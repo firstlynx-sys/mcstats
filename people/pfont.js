e0005235fc67fe0f004d87e91790afe810d6a1bc
firstlynx-sys <140927451+firstlynx-sys@users.noreply.github.com>
2024-04-11 23:45:43 -0400

    [ep[;people

---
+++ people/pfont.js
@@ -0,0 +1,6 @@
+//font changer
+var changeFontStyle = function (font) {
+            document.getElementById(
+          "output-text").style.fontFamily
+       = font.value;
+}
---
+++ people/pham.js
@@ -0,0 +1,6 @@
+//sticky hamburger
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
